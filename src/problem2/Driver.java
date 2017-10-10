7823c62dfdea449cb5ee36e92d385e1e70d09ec9
BlastoiseBlue <emmet@stanevich.com>
2017-10-10 13:46:34 -0400

    Added Fraction and Driver classes

---
+++ src/problem2/Driver.java
@@ -0,0 +1,18 @@
+/*
+ * Emmet Stanevich
+ * Problem 2
+ * 10/10/17
+ */
+package problem2;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		Fraction demo = new Fraction();
+		demo.x = 20;
+		demo.y = 30;
+		System.out.println(demo.divide());
+		System.out.println(demo.ratio());
+	}
+
+}
--- src/problem2/Fraction.java
+++ src/problem2/Fraction.java
@@ -1,6 +1,29 @@
 package problem2;
 
 public class Fraction {
-	double x;
-	double y;
+	int x;
+	int y;
+	int GCD;
+
+	public void setX(int newX) {
+		x = newX;
+	}
+
+	public void setY(int newY) {
+		y = newY;
+	}
+
+	public double divide() {
+		return (((double) x) / ((double) y));
+	}
+
+	public String ratio() {
+		GCD = 1;
+		for (int i = 1; i <= x && i <= y; i++) {
+			if (x % i == 0 && y % i == 0 && i > GCD) {
+				GCD = i;
+			}
+		}
+		return (x / GCD + "/" + y / GCD);
+	}
 }
